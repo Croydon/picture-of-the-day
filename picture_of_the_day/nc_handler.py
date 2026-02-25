cafacf0fd6150ac589f7d4475b823c03d6def89a
Croydon <git@cr0ydon.com>
2026-02-25 08:26:56 +0100

    If caching=False also reload config and fix config as parameter usage

--- picture_of_the_day/nc_handler.py
+++ picture_of_the_day/nc_handler.py
@@ -10,18 +10,24 @@ _client = None
 # /remote.php/dav/photos/user/albums/<album_id>
 # apps/photos/api/v1/preview/id?etag=<>&x=512&y=512
 
-def nc_is_instance_reachable(core_config=config.config["core"], caching=True) -> bool:
+def nc_is_instance_reachable(core_config=None, caching=True) -> bool:
     global _nc_instance_reachable;
     global _auth;
     global _client;
+
+    if core_config is None:
+        if not caching:
+            config.load_core_config()
+        core_config = config.config["core"]
+
     if caching and _nc_instance_reachable is not None:
         return _nc_instance_reachable
 
     try:
-        client = webdavclient({"webdav_hostname": config.config["core"]["nc_url"], "webdav_login": config.config["core"]["nc_username"], "webdav_password": config.config["core"]["nc_accesstoken"]})
+        client = webdavclient({"webdav_hostname": core_config["nc_url"], "webdav_login": core_config["nc_username"], "webdav_password": core_config["nc_accesstoken"]})
 
-        if client.check(f"/remote.php/dav/photos/{config.config["core"]["nc_username"]}/albums/"):
-            if client.list(f"/remote.php/dav/photos/{config.config["core"]["nc_username"]}/albums/"):
+        if client.check(f"/remote.php/dav/photos/{core_config["nc_username"]}/albums/"):
+            if client.list(f"/remote.php/dav/photos/{core_config["nc_username"]}/albums/"):
                 _nc_instance_reachable = True
                 _client = client
                 return True
