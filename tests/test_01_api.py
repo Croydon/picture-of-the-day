90c32dbe38095a758e7e8da75c37cc8776011b03
Croydon <git@cr0ydon.com>
2026-03-03 13:07:56 +0100

    Fix tests

--- tests/test_01_api.py
+++ tests/test_01_api.py
@@ -48,7 +48,7 @@ def test_api_get_pod_photo_bytes(monkeypatch):
     
     # without overlay: ca85488124e60afb8078dc44e9693b934f72c61c21be0714954da2c27409caad
     
-    assert "a9370dd6d16383aa0e431fdc6997ccf4148622848fa706866725b4c3d4778a43" == checksum
+    assert "7ba1c17f6dec3a5b6afd9c4c5680f121fde0e99bc3abfdedb87e8fdb57b3567c" == checksum
 
 def test_api_endpoint_calls_unauthenticated(monkeypatch):
     monkeypatch.chdir(os.path.join("tests", "configs", "is_admin_initialized_true"))
